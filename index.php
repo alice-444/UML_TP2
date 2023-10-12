ffa397c3d8e91b926dbfbfeb74f497779df9658e
alice-444 <91722431+alice-444@users.noreply.github.com>
2023-10-12 17:03:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $compte1->depot(350);
 $compte1->retrait(200);
 $compte1->show();
 
-$compte2 = new CompteBancaire($name, $value);
+$compte2 = new CompteBancaire($name, $balance);
 $compte2->depot(25);
 $compte2->show();
 
